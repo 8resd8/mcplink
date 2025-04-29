475fc6ad290e4e6b8a06a619af804abc38d2ed4f
방승윤 <sybang98@gmail.com>
2025-04-29 12:52:46 +0900

    Add desktop application

---
+++ app/src/lib/components/select.svelte
@@ -0,0 +1,7 @@
+<select class="select select-bordered rounded-[10px]">
+    <option disabled selected>AI Agent</option>
+    <option>Claude App</option>
+    <option>Cursor</option>
+    <option>Windsurf</option>
+    <option>Custom</option>
+  </select>
