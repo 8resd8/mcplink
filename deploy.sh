3cc56dd2295c704da5f7fd536e1617acb122cc9a
김현우 <dkanfjgwls@naver.com>
2025-04-29 22:10:45 +0900

    test: jenkins ci-cd 16 setting

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,10 @@ IMAGE_TO_DEPLOY="$1"
 
 if [ -z "$IMAGE_TO_DEPLOY" ]; then
   echo "오류: 배포할 Docker 이미지 이름이 지정되지 않았습니다."
-  echo "사용법: ./deploy.sh <이미지_전체_이름>"
   exit 1
 fi
 
 echo "==== 배포 시작: ${IMAGE_TO_DEPLOY} ===="
-
 
 echo "Docker Image pull... ${IMAGE_TO_DEPLOY}"
 docker pull "${IMAGE_TO_DEPLOY}" || { echo "오류: 이미지 풀(pull) 실패"; exit 1; }
