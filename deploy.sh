1debb803875fa71245fd5dc7ba30df3f31b270b8
김현우 <dkanfjgwls@naver.com>
2025-04-30 11:12:06 +0900

    test: jenkins ci-cd 24 setting

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,15 @@ set -e
 IMAGE_TO_DEPLOY="${BACKEND_IMAGE_TAG}"
 
 if [ -z "$IMAGE_TO_DEPLOY" ]; then
-  echo "오류: 배포할 Docker 이미지 이름이 지정되지 않았습니다."
+  echo "오류: 배포할 Docker 이미지 태그(BACKEND_IMAGE_TAG 환경 변수)가 지정되지 않았습니다."
   exit 1
 fi
 
+FULL_IMAGE_NAME="resd/backend:${IMAGE_TO_DEPLOY}"
 echo "==== 배포 시작: ${IMAGE_TO_DEPLOY} ===="
 
 echo "Docker Image pull... ${IMAGE_TO_DEPLOY}"
-docker pull "${IMAGE_TO_DEPLOY}" || { echo "오류: 이미지 풀(pull) 실패"; exit 1; }
-
+docker pull "${FULL_IMAGE_NAME}" || { echo "오류: 이미지 풀(${FULL_IMAGE_NAME}) 실패"; exit 1; }
 
 echo "기존 컨테이너 중지..."
 docker compose down
