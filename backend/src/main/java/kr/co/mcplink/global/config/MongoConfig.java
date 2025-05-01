1521ddb2d3f3038e8d0fb4d90a33a4a7c1b736f4
OiPKL <dhkswls1998@naver.com>
2025-05-01 23:13:11 +0900

    feat: add config

--- backend/src/main/java/kr/co/mcplink/global/config/MongoConfig.java
+++ backend/src/main/java/kr/co/mcplink/global/config/MongoConfig.java
@@ -1,4 +1,23 @@
 package kr.co.mcplink.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MongoConfig {
+
+    @Value("${spring.data.mongodb.host}")
+    private String mongoHost;
+
+    @Value("${spring.data.mongodb.port}")
+    private int mongoPort;
+
+    @Value("${spring.data.mongodb.database}")
+    private String mongoDb;
+
+    @Value("${spring.data.mongodb.username}")
+    private String mongoUser;
+
+    @Value("${spring.data.mongodb.password}")
+    private String mongoPassword;
 }
