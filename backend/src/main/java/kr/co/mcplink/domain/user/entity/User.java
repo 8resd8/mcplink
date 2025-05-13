065a703b524d14b49cf2d8a879f9dba392740ec7
김현우 <dkanfjgwls@naver.com>
2025-05-13 10:28:13 +0900

    feat: create userEntity, repository

---
+++ backend/src/main/java/kr/co/mcplink/domain/user/entity/User.java
@@ -0,0 +1,26 @@
+package kr.co.mcplink.domain.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import kr.co.mcplink.global.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Long id;
+
+
+}
