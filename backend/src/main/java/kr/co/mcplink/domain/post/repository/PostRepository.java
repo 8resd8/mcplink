2c6044635345d85c6814c039dfd7960baab9b248
김현우 <dkanfjgwls@naver.com>
2025-05-14 17:40:19 +0900

    feat: avoid proxyUser

--- backend/src/main/java/kr/co/mcplink/domain/post/repository/PostRepository.java
+++ backend/src/main/java/kr/co/mcplink/domain/post/repository/PostRepository.java
@@ -1,13 +1,23 @@
 package kr.co.mcplink.domain.post.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import kr.co.mcplink.domain.post.entity.Post;
 import kr.co.mcplink.domain.user.entity.User;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-	Optional<Post> findByIdAndUser(Long postId, User user);
+	@Query("select p from Post p join fetch p.user where p.id = :postId and p.user = :user")
+	Optional<Post> findByIdAndUser(@Param("postId") Long postId, @Param("user") User user);
+
+	Optional<Post> findPostWithUserById(Long postId);
+
+	@Query("select distinct p from Post p join fetch p.user")
+	List<Post> findAllWithUser();
+
 }
