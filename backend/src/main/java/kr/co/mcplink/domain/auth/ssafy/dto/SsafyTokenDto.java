da7d22babb889e2904b26135b3d06c095295d766
김현우 <dkanfjgwls@naver.com>
2025-05-15 13:13:22 +0900

    feat: ssafy-login guide dto

---
+++ backend/src/main/java/kr/co/mcplink/domain/auth/ssafy/dto/SsafyTokenDto.java
@@ -0,0 +1,23 @@
+package kr.co.mcplink.domain.auth.ssafy.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record SsafyTokenDto(
+	@JsonProperty("token_type")
+	String tokenType,
+
+	@JsonProperty("access_token")
+	String accessToken,
+
+	String scope,
+
+	@JsonProperty("expires_in")
+	String expiresIn,
+
+	@JsonProperty("refresh_token")
+	String refreshToken,
+
+	@JsonProperty("refresh_token_expires_in")
+	Integer refreshTokenExpiresIn
+) {
+}
