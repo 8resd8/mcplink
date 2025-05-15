f83e219fb661c6cdc4e9602e5878c67898acc543
김현우 <dkanfjgwls@naver.com>
2025-05-15 13:46:52 +0900

    feat: add login annotation

--- backend/src/main/java/kr/co/mcplink/global/config/WebConfig.java
+++ backend/src/main/java/kr/co/mcplink/global/config/WebConfig.java
@@ -1,41 +1,54 @@
 package kr.co.mcplink.global.config;
 
+import java.util.List;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import kr.co.mcplink.global.filter.JwtAuthenticationFilter;
+import kr.co.mcplink.global.resolver.LoginArgumentResolver;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://mcplink.co.kr", "http://localhost:5500")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                .allowedHeaders("*")
-                .exposedHeaders("Authorization")
-                .allowCredentials(true);
-    }
+	private final LoginArgumentResolver loginArgumentResolver;
 
-    @Override
-    public void configureMessageConverters(java.util.List<HttpMessageConverter<?>> converters) {
-        converters.add(new MappingJackson2HttpMessageConverter());
-    }
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("https://mcplink.co.kr", "http://localhost:5500")
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+			.allowedHeaders("*")
+			.exposedHeaders("Authorization")
+			.allowCredentials(true);
+	}
 
-    @Bean
-    public FilterRegistrationBean<JwtAuthenticationFilter> jwtAuthenticationFilterRegistration(JwtAuthenticationFilter jwtAuthenticationFilter) {
-        FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(jwtAuthenticationFilter);
-        // 모든 /api 경로 또는 전체 경로에 대해 필터가 일단 동작하도록 설정
-        registrationBean.addUrlPatterns("/api/*"); // 또는 "/*"
-        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE + 1);
-        return registrationBean;
-    }
+	@Override
+	public void configureMessageConverters(java.util.List<HttpMessageConverter<?>> converters) {
+		converters.add(new MappingJackson2HttpMessageConverter());
+	}
+
+	@Bean
+	public FilterRegistrationBean<JwtAuthenticationFilter> jwtAuthenticationFilterRegistration(
+		JwtAuthenticationFilter jwtAuthenticationFilter) {
+		FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
+		registrationBean.setFilter(jwtAuthenticationFilter);
+		registrationBean.addUrlPatterns("/api/*");
+		registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE + 1);
+		return registrationBean;
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(loginArgumentResolver);
+	}
 }
