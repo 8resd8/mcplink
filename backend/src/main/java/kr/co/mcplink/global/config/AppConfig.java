b8a3b82b59073cd3043ccc0e0696f6cd1bd62c6c
김현우 <dkanfjgwls@naver.com>
2025-05-16 16:23:01 +0900

    feat: change method Class

--- backend/src/main/java/kr/co/mcplink/global/config/AppConfig.java
+++ backend/src/main/java/kr/co/mcplink/global/config/AppConfig.java
@@ -1,8 +1,11 @@
 package kr.co.mcplink.global.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import kr.co.mcplink.global.filter.JwtAuthenticationFilter;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +13,15 @@ public class AppConfig {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@Bean
+	public FilterRegistrationBean<JwtAuthenticationFilter> jwtResist(
+		JwtAuthenticationFilter jwtAuthenticationFilter) {
+		FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
+		registrationBean.setFilter(jwtAuthenticationFilter);
+		registrationBean.addUrlPatterns("/*");
+		registrationBean.setOrder(1);
+		return registrationBean;
 	}
 }
--- backend/src/main/java/kr/co/mcplink/global/config/WebConfig.java
+++ backend/src/main/java/kr/co/mcplink/global/config/WebConfig.java
@@ -2,17 +2,13 @@ package kr.co.mcplink.global.config;
 
 import java.util.List;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import kr.co.mcplink.global.filter.JwtAuthenticationFilter;
 import kr.co.mcplink.global.resolver.LoginArgumentResolver;
 import lombok.RequiredArgsConstructor;
 
@@ -35,16 +31,6 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void configureMessageConverters(java.util.List<HttpMessageConverter<?>> converters) {
 		converters.add(new MappingJackson2HttpMessageConverter());
-	}
-
-	@Bean
-	public FilterRegistrationBean<JwtAuthenticationFilter> jwtAuthenticationFilterRegistration(
-		JwtAuthenticationFilter jwtAuthenticationFilter) {
-		FilterRegistrationBean<JwtAuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
-		registrationBean.setFilter(jwtAuthenticationFilter);
-		registrationBean.addUrlPatterns("/api/*");
-		registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE + 1);
-		return registrationBean;
 	}
 
 	@Override
