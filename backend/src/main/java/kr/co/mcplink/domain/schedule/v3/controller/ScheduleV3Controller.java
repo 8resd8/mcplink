e415faacdc93f3fb3bca82b63dc5f2af57a2c326
OiPKL <dhkswls1998@naver.com>
2025-05-17 17:01:03 +0900

    feat: implements controller v3

--- backend/src/main/java/kr/co/mcplink/domain/schedule/v3/controller/ScheduleV3Controller.java
+++ backend/src/main/java/kr/co/mcplink/domain/schedule/v3/controller/ScheduleV3Controller.java
@@ -1,8 +1,9 @@
 package kr.co.mcplink.domain.schedule.v3.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import kr.co.mcplink.domain.schedule.v3.service.ScheduleV3Service;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Schedule API v3", description = "⚠️ Do Not Use!! Only For OiPKL!! ⚠️")
 public class ScheduleV3Controller {
 
+    private final ScheduleV3Service scheduleService;
+
+    @PostMapping("/init")
+    public void initData() {
+
+        scheduleService.initData();
+    }
+
+    @PostMapping("/prep")
+    public void prepData() {
+
+        scheduleService.prepData();
+    }
+
+    @PostMapping("/update")
+    public void updateData() {
+
+        scheduleService.updateData();
+    }
 }
