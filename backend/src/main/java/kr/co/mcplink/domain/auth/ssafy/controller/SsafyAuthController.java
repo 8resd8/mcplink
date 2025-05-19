28e3ef3ab1c6dc5ca12187ebec420ddaba5b0783
김현우 <dkanfjgwls@naver.com>
2025-05-19 17:18:01 +0900

    refactor: Modifying code according to responsibility

--- backend/src/main/java/kr/co/mcplink/domain/auth/ssafy/controller/SsafyAuthController.java
+++ backend/src/main/java/kr/co/mcplink/domain/auth/ssafy/controller/SsafyAuthController.java
@@ -3,10 +3,7 @@ package kr.co.mcplink.domain.auth.ssafy.controller;
 import static kr.co.mcplink.global.common.Constants.*;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletResponse;
-import kr.co.mcplink.domain.auth.ssafy.dto.LoginResponse;
 import kr.co.mcplink.domain.auth.ssafy.service.SsafyAuthService;
 import kr.co.mcplink.global.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
@@ -35,20 +31,9 @@ public class SsafyAuthController {
 	}
 
 	@GetMapping("/ssafy/callback")
-	public void handleSsafyCallback(@RequestParam("code") String code,
-		HttpServletResponse httpServletResponse) throws IOException {
-		LoginResponse response = ssafyAuthService.processSsafyLogin(code);
-
-		ResponseCookie accessTokenCookie = ResponseCookie.from(ACCESS_TOKEN_NAME, response.accessToken())
-			.maxAge(TimeUnit.MILLISECONDS.toSeconds(response.accessExpiredAt()))
-			.path("/")
-			.secure(true)
-			.httpOnly(true)
-			.sameSite("LAX")
-			.build();
-
-		httpServletResponse.addHeader(HttpHeaders.SET_COOKIE, accessTokenCookie.toString());
-		httpServletResponse.sendRedirect("https://mcplink.co.kr");
+	public void handleSsafyCallback(@RequestParam("code") String code, HttpServletResponse httpResponse) throws
+		IOException {
+		ssafyAuthService.processSsafyLogin(code, httpResponse);
 	}
 
 	@PostMapping("/logout")
