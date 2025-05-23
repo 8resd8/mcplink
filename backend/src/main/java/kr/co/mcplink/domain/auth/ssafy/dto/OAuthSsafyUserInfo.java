69dac7f48a6ba324e6e55468e88e6128f8b2246d
김현우 <dkanfjgwls@naver.com>
2025-05-23 21:06:19 +0900

    feat: UserInfo record

---
+++ backend/src/main/java/kr/co/mcplink/domain/auth/ssafy/dto/OAuthSsafyUserInfo.java
@@ -0,0 +1,20 @@
+package kr.co.mcplink.domain.auth.ssafy.dto;
+
+import kr.co.mcplink.domain.user.dto.OAuthUserInfo;
+
+public record OAuthSsafyUserInfo(SsafyUserInfoDto userInfo) implements OAuthUserInfo {
+	@Override
+	public String getProviderId() {
+		return userInfo.ssafyUserId();
+	}
+
+	@Override
+	public String getEmail() {
+		return userInfo.email();
+	}
+
+	@Override
+	public String getName() {
+		return userInfo.name();
+	}
+}
