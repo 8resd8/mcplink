011cf183cd7c3b7185be76495dfdff2529dfbb69
김현우 <dkanfjgwls@naver.com>
2025-05-23 23:07:59 +0900

    refactor: remove unused method

--- backend/src/main/java/kr/co/mcplink/domain/post/entity/Post.java
+++ backend/src/main/java/kr/co/mcplink/domain/post/entity/Post.java
@@ -1,9 +1,5 @@
 package kr.co.mcplink.domain.post.entity;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -13,9 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import kr.co.mcplink.domain.comment.entity.Comment;
 import kr.co.mcplink.domain.user.entity.User;
 import kr.co.mcplink.global.common.BaseTimeEntity;
 import lombok.AccessLevel;
@@ -51,8 +45,6 @@ public class Post extends BaseTimeEntity {
 	@Column(nullable = false, columnDefinition = "integer default 0")
 	private Integer viewCount = 0;
 
-	@OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-	private List<Comment> comments = new ArrayList<>();
 
 	@Builder
 	private Post(String title, String content, User user) {
@@ -73,16 +65,6 @@ public class Post extends BaseTimeEntity {
 	public void updatePost(String title, String content) {
 		this.title = title == null ? this.title : title;
 		this.content = content == null ? this.content : content;
-	}
-
-	public void incrementLikeCount() {
-		this.likeCount++;
-	}
-
-	public void decrementLikeCount() {
-		if (this.likeCount > 0) {
-			this.likeCount--;
-		}
 	}
 
 	public void incrementViewCount() {
