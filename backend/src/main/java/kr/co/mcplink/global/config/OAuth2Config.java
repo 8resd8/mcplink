20286e78fad62049664c2c2cb39b2fed32e711a9
김현우 <dkanfjgwls@naver.com>
2025-05-23 20:58:02 +0900

    feat: add bean Provider

---
+++ backend/src/main/java/kr/co/mcplink/global/config/OAuth2Config.java
@@ -0,0 +1,22 @@
+package kr.co.mcplink.global.config;
+
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import kr.co.mcplink.domain.auth.ssafy.service.OAuthRedirectService;
+import kr.co.mcplink.global.common.SocialProvider;
+
+@Configuration
+public class OAuth2Config {
+
+	@Bean
+	public Map<SocialProvider, OAuthRedirectService> oauth2RedirectServices(List<OAuthRedirectService> services) {
+		Map<SocialProvider, OAuthRedirectService> redirectServices = new EnumMap<>(SocialProvider.class);
+		services.forEach(service -> redirectServices.put(service.getProvider(), service));
+		return redirectServices;
+	}
+}
