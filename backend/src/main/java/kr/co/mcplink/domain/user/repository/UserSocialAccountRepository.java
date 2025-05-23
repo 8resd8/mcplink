f3aaf2a386dc0d21342feccf942ed7428e94fd6d
김현우 <dkanfjgwls@naver.com>
2025-05-23 21:05:56 +0900

    feat: socialAccount Repository

---
+++ backend/src/main/java/kr/co/mcplink/domain/user/repository/UserSocialAccountRepository.java
@@ -0,0 +1,13 @@
+package kr.co.mcplink.domain.user.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kr.co.mcplink.domain.user.entity.UserSocialAccount;
+import kr.co.mcplink.global.common.SocialProvider;
+
+public interface UserSocialAccountRepository extends JpaRepository<UserSocialAccount, Long> {
+
+	Optional<UserSocialAccount> findByProviderAndProviderId(SocialProvider provider, String providerId);
+}
